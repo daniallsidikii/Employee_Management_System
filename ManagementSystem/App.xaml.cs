a855af46f9179cc448d68c1580dd0b5ec61f225b
daniyall_sidikii <160391502+daniallsidikii@users.noreply.github.com>
2025-03-13 02:38:57 +0500

    Add files via upload

---
+++ ManagementSystem/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace OHSAdminPanel
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Directly open MainWindow
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
+
