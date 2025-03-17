f9543157d51161c23ebd868ab65311826a7c6da1
daniallsidikii <m.daniyal.siddiqui.100@gmail.com>
2025-03-18 02:34:58 +0500

    UI updation admin pannel and login window

--- ManagementSystem/src/obj/Debug/net9.0-windows/ManagementSystem.AssemblyInfo.cs
+++ ManagementSystem/src/obj/Debug/net9.0-windows/ManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+039b55a02f7ecca1660b2d64f41d8eaa1ed2ff34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee98d5fa9a47a70dae1c0909c12f746e02c7d795")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
